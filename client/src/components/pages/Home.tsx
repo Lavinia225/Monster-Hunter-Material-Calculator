a922da37df3fc9f8f3057e1d520338a042182923
lunaseifert <thepuddingdeity@gmail.com>
2024-03-18 16:16:39 -0500

    Basic home page setup

--- client/src/components/pages/Home.tsx
+++ client/src/components/pages/Home.tsx
@@ -1,8 +1,15 @@
 function Home (){
     return (
-        <p>
-            Welcome home
-        </p>
+        <div id='home'>
+            <h2>WIP</h2>
+            <p>This is a material calculator to help create builds in Monster Hunter Rise. In addition to giving you the
+                material count, it will tell you the sum defense, resistances, and decoration slots!
+            </p>
+            <h4>Plans: </h4>
+            <ul>
+                <li>Expand to include World sets.</li>
+            </ul>
+        </div>
     )
 }
 
