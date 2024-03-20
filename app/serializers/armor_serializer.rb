0aabe0873797d7c7d45ec15f31c97e55d2c2d57e
lunaseifert <thepuddingdeity@gmail.com>
2024-03-20 18:20:54 -0500

    Added cost to armor cereal

--- app/serializers/armor_serializer.rb
+++ app/serializers/armor_serializer.rb
@@ -1,5 +1,5 @@
 class ArmorSerializer < ActiveModel::Serializer
-  attributes :id, :name, :slot, :defense, :decor_one, :decor_two, :decor_three, :fire_res, :water_res, :thunder_res, :ice_res, :dragon_res
+  attributes :id, :name, :slot, :defense, :decor_one, :decor_two, :decor_three, :fire_res, :water_res, :thunder_res, :ice_res, :dragon_res, :cost
   has_many :skills
   has_many :materials
 end
